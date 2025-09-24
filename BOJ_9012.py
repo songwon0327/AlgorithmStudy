68e889431d8cd4ccc57f10447eac6c28683daf72
songwon Lee <songwon12495@gmail.com>
2025-09-24 17:36:19 +0900

    Update BOJ_9012.py

--- BOJ_9012.py
+++ BOJ_9012.py
@@ -2,11 +2,11 @@ n = int(input())
 
 for _ in range(n):
     cnt = 0 
-    vfs = input()
-    for i in range(len(vfs)):
-        if vfs[i:i+1] == '(': 
+    vps = input()
+    for i in range(len(vps)):
+        if vps[i:i+1] == '(': 
             cnt += 1
-        elif (vfs[i:i+1] == ')'):
+        elif (vps[i:i+1] == ')'):
             cnt -= 1
         if cnt < 0: #(보다 )가 먼저오면 무조건 vfs가 아니므로
             break
